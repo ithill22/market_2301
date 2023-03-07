c0f18d7a13a8891d45ef2dea3784ee7e8f191d20
Isaac Thill <ithill22@turing.edu>
2023-03-07 09:51:40 -0700

    feat: vender_names method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -27,4 +27,14 @@ RSpec.describe Market do
       expect(market.vendors).to eq([vendor1, vendor2, vendor3])
     end
   end
+
+  describe '#vendor_names' do
+    it 'can list the names of all the vendors in the vendors array' do
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+      expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
 end
