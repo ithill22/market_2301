e35d9b671c5d700b00f59959ecce6e0741f1490b
Isaac Thill <ithill22@turing.edu>
2023-03-07 09:12:10 -0700

    fest: add initialize method for item.rb

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,10 @@
 class Item
   
-  attr_read
+  attr_reader :name,
+              :price
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price].tr("$", "").to_f
+  end
 end
