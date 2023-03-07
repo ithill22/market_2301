52c70d90e9b55d9947aa6c4fc5bde06e889beb9d
Isaac Thill <ithill22@turing.edu>
2023-03-07 09:16:55 -0700

    feat: add initialize method for vendor

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Vendor do
   let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
   let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
-  let(vendor) {Vendor.new("Rockey Mountain Fresh")}
+  let(:vendor) {Vendor.new("Rocky Mountain Fresh")}
 
   describe '#initialize' do
     it 'can initialize' do
