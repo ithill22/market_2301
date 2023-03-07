c9df57a11a889d70644cedf9c18f0a36d3836805
Isaac Thill <ithill22@turing.edu>
2023-03-07 09:14:59 -0700

    test: add initialize for vendor_spec

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,4 +3,13 @@ require 'spec_helper'
 RSpec.describe Vendor do
   let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
   let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
+  let(vendor) {Vendor.new("Rockey Mountain Fresh")}
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(vendor).to be_instance_of(Vendor)
+      expect(vendor.name).to eq("Rocky Mountain Fresh")
+      expect(vendor.inventory).to eq({})
+    end
+  end
 end
