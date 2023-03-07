fcce524a58f482e6455b50240f8d7eab66280f70
Isaac Thill <ithill22@turing.edu>
2023-03-07 11:14:52 -0700

    feat: add total_inventory method

--- lib/market.rb
+++ lib/market.rb
@@ -23,7 +23,7 @@ class Market
   def vendors_that_sell(item)
     in_stock = []
     @vendors.each do |vendor|
-      if vendor.check_stock(item) > 0
+      if vendor.inventory.include?(item)
         in_stock << vendor
       end
     end
@@ -41,5 +41,34 @@ class Market
     end
     list_of_items.uniq.sort
   end
+
+  def total_inventory
+    total_inventory = Hash.new{|h, k| h[k] = {quantity: 0, vendors: []}}
+    @vendors.map do |vendor|
+      vendor.inventory.each do |item, quantity|
+        if !total_inventory.key?(item)
+          total_inventory[item]
+          @vendors.each do |vendor|
+            total_inventory[item][:quantity] += vendor.check_stock(item)
+          end
+        else
+        end
+        total_inventory[item][:vendors] = vendors_that_sell(item)
+      end
+    end
+    total_inventory
+  end
+
+  # def overstocked_items
+  #   overstocked = []
+  #   @vendors.each do |vendor|
+  #     vendor.inventory do |item, quantity|
+  #       if quantity > 50 && 
+  #         overstocked << item
+  #       end
+  #     end
+  #   end
+  #   overstocked
+  # end
   
 end
